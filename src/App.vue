b1f5066d34886df0418425e9aaf5df24443928f3
Stefan Aniculaesei <zarosse@gmail.com>
2024-02-05 22:56:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="app-container">
+   
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+.app-container {
+  background-image: url(~@/assets/bgr1.jpg);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
+
+
+
+
+
